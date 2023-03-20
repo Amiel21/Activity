6b2c81588896c34cdc64fae67081b3ec17a4e66d
Amiel21 <prt1227@dlsud.edu.ph>
2023-03-20 17:35:46 +0800

    230320

---
+++ indexActivity.js
@@ -0,0 +1,33 @@
+
+// Name: Paredes, Ralph Amiel T. Paredes - BCS22
+// Enabling Assessment - Main Activity | S-ITCS227LA
+
+console.log('Hello, Welcome to De La Salle University!');
+
+let firstName = "Amiel";
+let lastName = "Paredes";
+
+let age = 19;
+
+let hobbies = ['Music','Drawing','Online Games','Online Videos/Movies','Video/Picture Editing'];
+
+let homeAddress = {
+
+	houseNumber: 'B4 L34',
+	street: 'Vanda St.',
+	village: 'South Garden Homes',
+	barangay: 'Salitran 3',
+	city: 'Dasmarinas City',
+	province: 'Cavite',
+	zipcode: 4114
+
+};
+
+console.log(firstName, lastName);
+console.log(hobbies);
+console.log(homeAddress);
+
+
+
+
+
